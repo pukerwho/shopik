9267146a1a04eda454d41c22a962be04891bbc04
Vladimir Samojlov <v-moi@ya.ru>
2020-04-07 14:23:01 +0300

    edit footer

--- footer.php
+++ footer.php
@@ -1,4 +1,81 @@
 		</section>
+		<footer class="footer py-10 mt-12">
+			<div class="container mx-auto px-4 lg:px-0">
+				<div class="flex flex-col lg:flex-row mb-8">
+					<div class="w-full lg:w-1/3">
+						<h2 class="mb-4">
+							<?php _e('Каталог', 'shop'); ?>	
+						</h2>
+						<div>
+							<?php $categories = get_terms([
+								'taxonomy' => 'product_cat',
+								'hide_empty' => false,
+								'exclude' => array(15),
+								'parent' => 0,
+							]); 
+							foreach($categories as $category): ?>
+								<li class="list-none">
+									<a href="<?php echo get_term_link($category); ?>">
+										<?php echo $category->name; ?>
+									</a>	
+								</li>
+							<?php endforeach; ?>
+						</div>
+					</div>
+					<div class="w-full lg:w-1/3">
+						<h2 class="mb-4">
+							<?php _e('Клиенту', 'shop'); ?>	
+						</h2>
+						<div>
+							<li class="list-none">
+								<a href="#">Таблица размеров</a>
+							</li>
+						</div>
+					</div>
+					<div class="w-full lg:w-1/3">
+						<div class="mb-8">
+							<h2 class="text-center mb-4">
+								<?php _e('Контакты', 'shop'); ?>	
+							</h2>
+							<div class="flex mb-4">
+								<div class="w-1/4 mr-4">
+									<?php _e('Телефоны', 'shop'); ?>:
+								</div>
+								<div class="flex flex-col">
+									<a href="#">099-011-111-01</a>
+									<a href="#">099-011-111-01</a>
+									<a href="#">099-011-111-01</a>
+								</div>
+							</div>
+							<div class="flex mb-4">
+								<div class="w-1/4 mr-4">
+									<?php _e('Email', 'shop'); ?>:
+								</div>
+								<div class="flex flex-col">
+									<a href="#">email@email.com</a>
+								</div>
+							</div>
+							<div class="flex mb-4">
+								<div class="w-1/4 mr-4">
+									<?php _e('Адрес', 'shop'); ?>:
+								</div>
+								<div class="flex flex-col">
+									Украина, Область, Город
+								</div>
+							</div>
+						</div>
+						<div>
+							<div class="btn text-white text-center">
+								<?php _e('Заказать обратный звонок', 'shop'); ?>
+							</div>
+						</div>
+					</div>
+				</div>	
+				<div class="copyright text-center pt-8">
+					<?php _e('Разработка', 'shop'); ?>: <a href="https://timeto.top">TimeToTop</a>
+				</div>
+			</div>
+		</footer>
 	<?php wp_footer(); ?>
 	</body>
 </html>
